fdf193a13fa995b1e3caab489bce02c1da14807c
Albazos <moritz.reimann.02@web.de>
2025-11-27 14:54:06 +0100

    first commit

---
+++ IservMailManager.py
@@ -0,0 +1,25 @@
+from IServAPI import IServAPI as ISAPI
+
+class IservMailManager:
+    mUsername = None
+    mPassword = None
+    mURL = None
+    mAPIconnection = None
+    
+    def __init__(self, aUsername, aPassword, aURL):
+        self.mUsername = aUsername
+        self.mPassword = aPassword
+        self.mURL = aURL
+        self.mAPIconnection = ISAPI(self.mUsername,self.mPassword,self.mURL)
+        
+
+    def sendMail(self, aMailData):
+        
+        lFrom = self.mUsername + "@wvss.de" # type: ignore #to remove from list
+        #aMailData[0].remove(lFrom)
+        lTo = aMailData[0]
+        lHtmlBody = aMailData[1]
+
+        self.mAPIconnection.send_email(lTo,"Kuchenreminder",body="", html_body=lHtmlBody) # type: ignore
+        return True
+    
